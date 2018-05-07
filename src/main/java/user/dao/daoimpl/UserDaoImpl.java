ac333219126dd08a4719df0a005cbe82c623b87f
Your Namelilianggeng <fc@fengcheng.com>
2018-05-07 18:41:09 +0800

    Service interface

---
+++ src/main/java/user/dao/daoimpl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package user.dao.daoimpl;
+
+import user.dao.UserDao;
+
+public class UserDaoImpl implements UserDao{
+
+}
---
+++ src/main/java/user/service/serviceimpl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package user.service.serviceimpl;
+
+import user.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+}
