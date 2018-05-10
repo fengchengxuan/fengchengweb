af4f4090258fb91f4501bf6698473f79cd387c8e
Ann <120647516@qq.com>
2018-05-10 20:34:41 +0800

    commit

--- src/main/java/FreeAppNum/service/FreeAppNumService.java
+++ src/main/java/FreeAppNum/service/FreeAppNumService.java
@@ -1,7 +1,11 @@
 package FreeAppNum.service;
 
+import java.util.List;
+
+import freeapply.entity.FreeApplyNum;
+
 public interface FreeAppNumService {
-	void getFreeAppNum();//获取免费体验名额
+	List<FreeApplyNum> getFreeAppNum();//获取免费体验名额
 	void  findFreeAppNum(); //会员页-直达客服经理-免费体验名额
 	boolean authApply();//后台管理页-审批申请  
 }
