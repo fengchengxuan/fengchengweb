fbe933c5d25f52dac41885b66b13e18569ae1ae8
xuandafeng <786889913@qq.com>
2018-05-14 16:19:54 +0800

    login

--- src/main/webapp/vip/app.js
+++ src/main/webapp/vip/app.js
@@ -9,9 +9,30 @@ define(["angular", "angularAMD", "ui-router"], function(angular, angularAMD) {
         $stateProvider
             .state("viphome", angularAMD.route({
                 url: "/viphome",
+                params: {
+                    param1: null
+                },
                 templateUrl: "./viphome/html/viphome.html",
                 controller: "viphomeCtrl",
                 controllerUrl: "./viphome/js/viphomeCtrl.js",
+            }))
+            .state("updateUserInfo", angularAMD.route({
+                url: "/updateUserInfo",
+                templateUrl: "./vipuser/html/updateUserInfo.html",
+                controller: "baseCtrl",
+                controllerUrl: "./vipuser/js/baseCtrl.js",
+            }))
+            .state("vipmima", angularAMD.route({
+                url: "/vipmima",
+                templateUrl: "./vipuser/html/vipmima.html",
+                controller: "vipmimaCtrl",
+                controllerUrl: "./vipuser/js/vipmimaCtrl.js",
+            }))
+            .state("vipmibao", angularAMD.route({
+                url: "/vipmibao",
+                templateUrl: "./vipuser/html/vipmibao.html",
+                controller: "vipmibaoCtrl",
+                controllerUrl: "./vipuser/js/vipmibaoCtrl.js",
             }))
             .state("login", angularAMD.route({
                 url: "/login",
@@ -22,8 +43,8 @@ define(["angular", "angularAMD", "ui-router"], function(angular, angularAMD) {
             .state("register", angularAMD.route({
                 url: "/register",
                 templateUrl: "./vipuser/html/register.html",
-                controller: "viplrCtrl",
-                controllerUrl: "./vipuser/js/viplrCtrl.js",
+               /* controller: "viplrCtrl",
+                controllerUrl: "./vipuser/js/viplrCtrl.js",*/
             }))
     });
     return angularAMD.bootstrap(app);
--- src/main/webapp/vip/vipuser/js/viplrCtrl.js
+++ src/main/webapp/vip/vipuser/js/viplrCtrl.js
@@ -1,5 +1,15 @@
 define(["app"], function(app) {
-    return app.controller('viplrCtrl', function($rootScope, $scope) {
+    return app.controller('viplrCtrl', function($rootScope, $scope,$http) {
+        $rootScope.login = function(name, password) {
+            $scope.user ={
+                USER_NAME:name,
+                PASSWORD:password
+            };
+            // console.log(user);
+            $http.post('login/' ,{'user':JSON.stringify($scope.user)}).success(function(data) {
 
+            });
+
+        };
     });
 });
